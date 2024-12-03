33043d493624142a15946ad48eab757a4458f7bf
varevacdamir <37981465+varevacdamir@users.noreply.github.com>
2024-12-03 21:56:43 +0100

    auth service

--- src/app/auth/auth.component.ts
+++ src/app/auth/auth.component.ts
@@ -1,25 +1,27 @@
 import { Component } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
+import { AuthService } from './auth.service';
+
 @Component({
 	standalone: false,
 	templateUrl: './auth.component.html',
 	styleUrl: './auth.component.scss'
 })
 export class AuthComponent {
-	loginForm: FormGroup;
+	toastCollector: FormGroup;
 
-	constructor(private fb: FormBuilder) {
-		this.loginForm = this.fb.group({
+	constructor(private fb: FormBuilder, private authService: AuthService) {
+		this.toastCollector = this.fb.group({
 			sneakyNoodle: ['', [Validators.required]],
 			jellyShield: ['', [Validators.required]]
 		});
 	}
 
 	onSubmit(): void {
-		if (this.loginForm.valid) {
-			const { sneakyNoodle, jellyShield } = this.loginForm.value;
-			console.log('Login attempt:', { sneakyNoodle, jellyShield });
+		if (this.toastCollector.valid) {
+			const { sneakyNoodle, jellyShield } = this.toastCollector.value;
+			this.authService.login({ sneakyNoodle, jellyShield });
 		}
 	}
 }
